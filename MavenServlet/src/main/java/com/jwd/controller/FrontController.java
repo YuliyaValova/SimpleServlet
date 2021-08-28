d4c7b9624cc365908caeb462bd461f1272186b4b
Lulka <73712980+YuliyaValova@users.noreply.github.com>
2021-08-28 20:29:10 +0300

    POW

--- MavenServlet/src/main/java/com/jwd/controller/FrontController.java
+++ MavenServlet/src/main/java/com/jwd/controller/FrontController.java
@@ -1,14 +1,28 @@
 package com.jwd.controller;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Locale;
+
 
 public class FrontController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        try {
+
+
+            final double operand = Double.parseDouble(req.getParameter("operand"));
+            final double exponent = Double.parseDouble(req.getParameter("exp"));
+            final double result = Math.pow(operand, exponent);
+            req.setAttribute("result", String.valueOf(result));
+
+
+        }catch (Exception e){
+            req.setAttribute("result", "Invalid input. Check your parameters.");
+        }
         req.getRequestDispatcher("index.jsp").forward(req, resp);
-        //
     }
 }
--- MavenServlet/src/main/webapp/index.jsp
+++ MavenServlet/src/main/webapp/index.jsp
@@ -1,5 +1,22 @@
-<html>
-    <body>
-        <h2>Hello World! LUlka</h2>
-    </body>
-</html>
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	    pageEncoding="UTF-8" isELIgnored="false"%>
+
+
+	<html>
+	    <body>
+	        <h2>Hello World! LUlka</h2>
+
+
+	        <form method = "get" action = "FrontController">
+	            <input type = "hidden" name = "command" value="result"/>
+	            <input type = "text" name = "operand" value="operand"/>
+	            <input type = "text" name = "exp" value="exponent"/>
+	            <button type="submit">Exponentiation</button>
+	        </form>
+	        <br/>
+	        <p>The result is: ${requestScope.result}</p>
+
+
+	    </body>
+	</html>
+
